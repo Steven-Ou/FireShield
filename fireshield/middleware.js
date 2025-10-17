31d5347a8c26aae17e9253fd292729005d2f80b6
Steven-Ou <osteve425@gmail.com>
2025-10-17 15:56:50 -0400

    Fixing

--- fireshield/middleware.js
+++ fireshield/middleware.js
@@ -1,14 +1,18 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher([
-  "/dashboard(.*)", // Protect all routes starting with /dashboard
-]);
+const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
 
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) {
-    auth().protect(); // If the route is protected, ensure the user is authenticated
+export default clerkMiddleware(
+  (auth, req) => {
+    if (isProtectedRoute(req)) {
+      auth().protect();
+    }
+  },
+  {
+    // Add these lines to define public routes
+    publicRoutes: ["/", "/sign-in", "/sign-up"],
   }
-});
+);
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
