4e9efc060462039a9491964250cd8b80550b59f7
Steven-Ou <osteve425@gmail.com>
2025-10-18 10:07:58 -0400

    pushing in the new stuff

---
+++ Main/OnboardingView.swift
@@ -0,0 +1,13 @@
+//
+//  OnboardingView.swift
+//  Main
+//
+//  Created by Steve Ou on 10/18/25.
+//
+
+import SwiftUI
+
+struct OnboardingView: View{
+    @AppStorage("hasCompletedOnboarding") private var hasCompletedOnboarding = false
+}
+                        
