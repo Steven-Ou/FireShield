112d9f9f21af47d1099b39e33d82c543dfbebdd1
Steven-Ou <osteve425@gmail.com>
2025-10-18 11:14:08 -0400

    Fixing the bug

--- Main/MainApp.swift
+++ Main/MainApp.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 @main
 struct MainApp: App {
+    @AppStorage("hasCompletedOnboarding") private var hasCompletedOnboarding = false
+
     var body: some Scene {
         WindowGroup {
             if hasCompletedOnboarding {
