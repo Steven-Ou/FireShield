f0cdbf175eebaf45ee84c8be85b7013219968997
Steven-Ou <osteve425@gmail.com>
2025-10-18 11:51:19 -0400

    adding in the view Router

--- Main/ViewRouter.swift
+++ Main/ViewRouter.swift
@@ -1,7 +1,14 @@
-//
-//  Untitled.swift
-//  Main
-//
-//  Created by Steve Ou on 10/18/25.
-//
+import SwiftUI
+
+// This class will manage the navigation state of our app.
+class ViewRouter: ObservableObject {
+    // @Published will notify any views watching this property when it changes.
+    @Published var currentPage: Page = .onboarding
+}
+
+// An enum to define the different pages in our app.
+enum Page {
+    case onboarding
+    case login
+}
 
